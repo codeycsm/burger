03f613eabc50b847970690430a424e52f42f0e1e
Codey <codeymcdaniel@gmail.com>
2019-02-16 18:19:03 -0700

    updated sql connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 //   mysql package and connections
 let mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "bootcamp",
-  database: "burgers_db"
-});
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
+// {
+//   host: "pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     port: 3306,
+//       user: "npt7mpc0t3wc5dzv",
+//         password: "be4u6rundpj2gkd4",
+//           database: "kwd80ib9242z1vr7"
+// }
 module.exports = connection;
